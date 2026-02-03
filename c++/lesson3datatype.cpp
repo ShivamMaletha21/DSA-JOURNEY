f5b318cbd9206519756f0c813c536eefb0659d33
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-03 16:38:55 +0530

    data types

--- c++/lesson3datatype.cpp
+++ c++/lesson3datatype.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 main()
 {
-    // int a = 20;
-    // char b = 'A';
-    // string c = "Raj";
-    // float d = 45.7;
+    int a = 20;
+    char b = 'A';
+    string c = "Raj";
+    float d = 45.7;
 
-    // cout << a << endl
-    //      << b << endl
-    //      << c << endl
-    //      << d;
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << d;
 
-    // to check size we use.. sizeof();
+    // to check size we use..sizeof();
 
     cout << sizeof(int) << "bytes" << endl;
     cout << sizeof(char) << "bytes" << endl;
