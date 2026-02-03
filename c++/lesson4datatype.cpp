b5e754c9e7e5dc85ab441fa2c850889640bab3ef
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-03 16:32:58 +0530

    learn data types

---
+++ c++/lesson4datatype.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    // int a = 20;
+    // char b = 'A';
+    // string c = "Raj";
+    // float d = 45.7;
+
+    // cout << a << endl
+    //      << b << endl
+    //      << c << endl
+    //      << d;
+
+    // to check size we use.. sizeof();
+
+    cout << sizeof(int) << "bytes" << endl;
+    cout << sizeof(char) << "bytes" << endl;
+    cout << sizeof(string) << "bytes" << endl;
+    cout << sizeof(float) << "bytes" << endl;
+}
