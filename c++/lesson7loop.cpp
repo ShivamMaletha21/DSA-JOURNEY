b0631b7de1f6f72daafb7265344c2b42e4e1e76b
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-04 17:07:17 +0530

    Loops

---
+++ c++/lesson7loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// main()
+// {
+//     for (int i = 1; i <= 100; i++)
+//     {
+//         cout << "Shivam" << endl;
+//     }
+// }
+
+// do while
+main()
+{
+    int a = 1;
+    do
+    {
+        cout << a << endl;
+        ++a;
+    } while (a < 0);
+}
