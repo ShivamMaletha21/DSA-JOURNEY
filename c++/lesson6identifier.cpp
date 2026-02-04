6911d467c0c74a5d588d25f9fd1cddf0850c2466
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-04 16:33:35 +0530

    learn identifier

---
+++ c++/lesson6identifier.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+void Myname()
+{ // Myname is an identifier
+
+    cout << "arjun" << endl;
+}
+
+main()
+{
+    string name = "karan";
+    cout << name << endl;
+    Myname();
+}
