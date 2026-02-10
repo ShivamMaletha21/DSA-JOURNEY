6495c5e6804f8443ae2ad95eb6ea95a5e1662bdd
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-10 12:27:02 +0530

    learning structure

---
+++ c++/lesson16structure.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// creating structure
+struct Emp // data type.. This is a structure definition.. defining what an Emp looks like
+{
+    string name; // doesn’t store a value — it only defines a type.
+    int number;
+    double salary;
+};
+
+main()
+{
+    struct Emp E; // This line creates a variable. .. struct Emp -> type ,, E -> variable name
+    E.name = "Rahul";
+    E.number = 8083831;
+    E.salary = 500000;
+
+    cout << E.name << endl
+         << E.number << endl
+         << E.salary;
+}
