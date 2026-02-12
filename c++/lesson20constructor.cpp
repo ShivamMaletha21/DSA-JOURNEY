5fa53ba811ecec2d69aa804b6053490871c6825c
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-12 12:29:27 +0530

    Constructor

---
+++ c++/lesson20constructor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class A
+{
+public:    // optional
+    int a; // it is private by default or you  can also write private : then int a;
+    A()    // it is a constructor .. contain same name as of class name .. use to initialize data we does not use cout here bcz constructor is use to initialize data... if we dont use it ... compiler assign constructor byitself and fit a garbage value in int a;
+    {
+        a = 100;
+    }
+    void show() // member function... use to perform action
+    {
+        cout << a;
+    }
+};
+
+int main()
+{
+
+    A obj = A();
+    obj.show();
+    getch();
+    return 0;
+}
