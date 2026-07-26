30e1588e3bdcc97f23ed40e053a4a52d2ab32d9a
ShivamMaletha21 <malethashivam43@gmail.com>
2026-07-26 17:42:29 +0530

    Add C++ lesson 1

---
+++ c++_new_revise/lesson1.cpp
@@ -0,0 +1,56 @@
+// basic structure
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "I am learning c++";
+//     return 0;
+// }
+
+// // input / output
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int age;
+//     cout << "Enter your age: ";
+//     cin >> age;
+//     cout << "Your age is " << age << "\n";
+//     return 0;
+// }
+
+// Data Type
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int age = 22;
+
+    float height = 5.7f;
+
+    double pi = 3.14444343;
+
+    char grade = 'A';
+
+    bool isStudent = true;
+
+    std::string name = "Shivam Maletha";
+
+    auto score = 85;
+
+    const int MAX_SCORE = 100;
+
+    // printing
+    std::cout << "Name: " << name << "\n";
+    std::cout << "age: " << age << "\n";
+    std::cout << "height: " << height << "\n";
+    std::cout << "Grade: " << grade << "\n";
+    std::cout << "Student: " << isStudent << "\n";
+    std::cout << "Score: " << score << "\n";
+
+    return 0;
+}
