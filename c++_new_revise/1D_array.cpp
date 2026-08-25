499891c27b8a9a76d283fc19bcfcd77f72045de3
ShivamMaletha21 <malethashivam43@gmail.com>
2026-08-25 23:08:09 +0530

    learning c++

--- c++_new_revise/1D_array.cpp
+++ c++_new_revise/1D_array.cpp
@@ -146,3 +146,19 @@ int main()
     //     std::cout << min;
     //     return 0;
     // }
+
+#include <iostream>
+    using namespace std;
+
+    int main()
+    {
+
+        int nums[5] = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < 5; i++)
+        {
+            cout << nums[i] << endl;
+        }
+
+        return 0;
+    }
