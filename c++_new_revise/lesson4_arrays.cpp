7470dc7f4b9ed81c8bf5b66d4e20eb4f4457d853
ShivamMaletha21 <malethashivam43@gmail.com>
2026-08-26 14:07:30 +0530

    finished arrays 1D and 2D arrays in c++

--- c++_new_revise/lesson4_arrays.cpp
+++ c++_new_revise/lesson4_arrays.cpp
@@ -2,6 +2,7 @@
 
 // Q1. Create and Print
 // WAP to create an integer array of size 5, store 5 numbers in it, and print all the elements.
+
 #include <iostream>
 using namespace std;
 
@@ -119,3 +120,116 @@ using namespace std;
 // 1 2 3
 // 4 5 6
 // 7 8 9
+
+// int main()
+// {
+//     int arr[3][3] = {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9}};
+
+//     for (int i = 0; i < 3; i++)
+//     {
+
+//         for (int j = 0; j < 3; j++)
+//         {
+//             std::cout << arr[i][j] << " ";
+//         }
+//         std ::cout << "\n";
+//     }
+//     return 0;
+// }
+
+// Q8. Take Input in 2D Array
+// WAP to create a 3 × 3 integer array and take all 9 values from the user using nested loops.
+// Then print the matrix.
+
+// int main()
+// {
+//     int arr[3][3];
+
+//     // input
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             cin >> arr[i][j];
+//         }
+//         cout << "\n";
+//     }
+
+//     // output
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             cout << arr[i][j];
+//         }
+//         cout << "\n";
+//     }
+
+//     return 0;
+// }
+
+// Q9. Access Specific Elements
+// For a 3 × 3 array, WAP to print:
+// First element
+// Center element
+// Last element
+
+// Think carefully about their [row][column] indexes.
+
+// int main()
+// {
+//     int arr[3][3] = {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9}};
+
+//     // first element
+//     cout << arr[0][0];
+
+//     cout << "\n";
+
+//     // center element
+//     cout << arr[1][1];
+
+//     cout << "\n";
+
+//     // last element
+//     cout << arr[2][2];
+
+//     return 0;
+// }
+
+// Q. Sum of All Elements
+// WAP to take values in a 3 × 3 array and calculate the sum of all 9 elements using nested loops.
+// Input
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// Output:
+// 45
+
+int main()
+{
+
+    int sum = 0;
+
+    int arr[3][3];
+
+    // taking input
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+
+            sum += arr[i][j];
+        }
+    }
+    cout << sum;
+
+    return 0;
+}
